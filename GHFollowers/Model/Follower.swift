d8039ae3f8da47008301391733fd0e353c1355a9
Stefan Liu <bill72614321@gmail.com>
2024-01-22 22:33:57 -0800

    API, JSON, Model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by 劉力源 on 1/1/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
